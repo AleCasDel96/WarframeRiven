da3a499c582576eec21c49f3e0dd91e0bc3cb016
Alejandro Castrillo Delgado <alecasdel96@gmail.com>
2025-05-22 14:50:28 +0100

    cambio main

--- Frontend/WarRivenApp/src/app/app.config.ts
+++ Frontend/WarRivenApp/src/app/app.config.ts
@@ -1,15 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { AuthInterceptor } from './auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(
-      withFetch(), 
-      withInterceptors([AuthInterceptor])
-    ),
     provideRouter(routes)
   ]
 };
--- Frontend/WarRivenApp/src/main.ts
+++ Frontend/WarRivenApp/src/main.ts
@@ -1,12 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './app/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(withInterceptors([AuthInterceptor])),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([AuthInterceptor])
+    ),
     ...appConfig.providers
   ]
 });
