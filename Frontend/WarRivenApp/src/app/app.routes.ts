31063b483c64d1de727d8421603d91b1b6002593
Alejandro Castrillo Delgado <alecasdel96@gmail.com>
2025-05-22 14:31:43 +0100

    reincorporado guards

--- Frontend/WarRivenApp/src/app/app.routes.ts
+++ Frontend/WarRivenApp/src/app/app.routes.ts
@@ -1,53 +1,67 @@
 import { Routes } from '@angular/router';
+import { noAuthGuard } from './guards/no-auth.guard';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./inicio/inicio.component').then(m => m.InicioComponent)
+    loadComponent: () => import('./inicio/inicio.component').then(m => m.InicioComponent),
+    canActivate: [noAuthGuard]
   },
   {
     path: 'login',
-    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+    canActivate: [noAuthGuard]
   },
   {
     path: 'registro',
-    loadComponent: () => import('./registro/registro.component').then(m => m.RegistroComponent)
+    loadComponent: () => import('./registro/registro.component').then(m => m.RegistroComponent),
+    canActivate: [noAuthGuard]
   },
   {
     path: 'perfil',
-    loadComponent: () => import('./perfil/perfil.component').then(m => m.PerfilComponent)
+    loadComponent: () => import('./perfil/perfil.component').then(m => m.PerfilComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'crear-riven',
-    loadComponent: () => import('./crear-riven/crear-riven.component').then(m => m.CrearRivenComponent)
+    loadComponent: () => import('./crear-riven/crear-riven.component').then(m => m.CrearRivenComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'editar-riven/:id',
-    loadComponent: () => import('./editar-riven/editar-riven.component').then(m => m.EditarRivenComponent)
+    loadComponent: () => import('./editar-riven/editar-riven.component').then(m => m.EditarRivenComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'crear-oferta/:idRiven',
-    loadComponent: () => import('./crear-oferta/crear-oferta.component').then(m => m.CrearOfertaComponent)
+    loadComponent: () => import('./crear-oferta/crear-oferta.component').then(m => m.CrearOfertaComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'ofertas-publicas',
-    loadComponent: () => import('./ofertas-publicas/ofertas-publicas.component').then(m => m.OfertasPublicasComponent)
+    loadComponent: () => import('./ofertas-publicas/ofertas-publicas.component').then(m => m.OfertasPublicasComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'mis-ofertas',
-    loadComponent: () => import('./mis-ofertas/mis-ofertas.component').then(m => m.MisOfertasComponent)
+    loadComponent: () => import('./mis-ofertas/mis-ofertas.component').then(m => m.MisOfertasComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'mis-ventas',
-    loadComponent: () => import('./mis-ventas/mis-ventas.component').then(m => m.MisVentasComponent)
+    loadComponent: () => import('./mis-ventas/mis-ventas.component').then(m => m.MisVentasComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'historial',
-    loadComponent: () => import('./historial/historial.component').then(m => m.HistorialComponent)
+    loadComponent: () => import('./historial/historial.component').then(m => m.HistorialComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'rivens',
-    loadComponent: () => import('./rivens/rivens.component').then(m => m.RivensComponent)
+    loadComponent: () => import('./rivens/rivens.component').then(m => m.RivensComponent),
+    canActivate: [authGuard]
   },
   {
     path: '**',
